7daeb075a962a1606e52bba63fef05a63eb5e19c
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-01-18 13:39:27 -0300

    commit

--- 6-fetch/ProjetoComApi/script.js
+++ 6-fetch/ProjetoComApi/script.js
@@ -1,14 +1,14 @@
 // a função async sinaliza que oque vai acontecer é assincrono.
 const qlq = new URLSearchParams(window.location.search)
 const posid = qlq.get('id')
+const url = "https://jsonplaceholder.typicode.com/posts"
+
 
 async function getDataPost(){
 
-  const url = "https://jsonplaceholder.typicode.com/posts"
 
   // o away deve ser usado sempre onde tiver função assincrona, como await e fetch
   const response = await fetch(url)
-
   const data = await response.json()
 
   const gerarPost = dados =>{
@@ -26,7 +26,7 @@ async function getDataPost(){
 
       titulo.innerText = data[i].title
       paragrafo.innerText = data[i].body
-      btn.setAttribute('href', `posts.html?id= ${data[i].id}`)
+      btn.setAttribute('href', `posts.html?id=${data[i].id}`)
     
       div.appendChild(titulo)
       div.appendChild(paragrafo)
@@ -34,15 +34,79 @@ async function getDataPost(){
       posts.innerHTML += div2.innerHTML
       posts.append(btn)
     }
-
   }
   gerarPost()
 }
+function createComment(comment){
 
-async function gerarComentarios(){
+  let posts = document.querySelector('#posts')
+  let div = document.createElement('div')
+  let titulo = document.createElement('h2')
+  let paragrafo = document.createElement('p')
 
+  
+  titulo.innerText = comment.name
+  paragrafo.innerText = comment.body
+
+  div.appendChild(titulo)
+  div.appendChild(paragrafo)
+  posts.appendChild(div)
 }
-if(!posid)getDataPost()
+
+async function gerarComment(response){
+  let comments = await response.json()
+
+  comments.map(createComment)
+}
+
+async function inserirComentario(comentario){
+  const res = await fetch(`${url}/${posid}/comments/`,{
+    method: 'POST',
+    body: comentario,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+
+  const data = await res.json()
+  createComment(data)
+}
+
+async function gerarPagina(id){
+  const responseComments = await fetch(`${url}/${id}/comments/`)
+
+  gerarComment(responseComments)
+  
+  
+  let form = document.querySelector('#form')
+  let loading = document.querySelector('.loading')
+  
+  form.classList.remove('hide')
+  loading.classList.add('hide')
+  
+  form.addEventListener('submit', e =>{
+    e.preventDefault()
+
+    let name = document.querySelector('#email').value
+    let body = document.querySelector('#body').value
+    
+    let comentario = {
+      name,
+      body,
+    }
+
+    comentario = JSON.stringify(comentario)
+
+    inserirComentario(comentario)
+
+    document.querySelector('#email').value = ''
+    document.querySelector('#body').value = ''
+
+  })  
+}
+if(!posid){
+  getDataPost()
+}
 else{
-  gerarComentarios()
+  gerarPagina(posid)
 }
